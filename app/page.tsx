14242ada50882317605f6db2c24bb1f80db7bba8
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-06-29 11:03:04 -0500

    feat(home): add locations section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HomeHero } from "@/components/home/hero";
+import { HomeLocations } from "@/components/home/locations";
 import { HomeNavbar } from "@/components/home/navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col bg-background text-primary">
       <HomeNavbar />
       <HomeHero />
+      <HomeLocations />
     </main>
   );
 }
