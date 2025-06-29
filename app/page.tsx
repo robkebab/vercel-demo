e810d55e38ce64e2b38bc174664f3398fa2762f6
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-06-29 13:03:10 -0500

    feat(home): add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { HomeAbout } from "@/components/home/about";
 import { HomeCatering } from "@/components/home/catering";
+import { HomeFooter } from "@/components/home/footer";
 import { HomeHero } from "@/components/home/hero";
 import { HomeLocations } from "@/components/home/locations";
 import { HomeNavbar } from "@/components/home/navbar";
@@ -7,13 +8,16 @@ import { HomeReviews } from "@/components/home/reviews";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col bg-background text-primary">
-      <HomeNavbar />
-      <HomeHero />
-      <HomeLocations />
-      <HomeAbout />
-      <HomeReviews />
-      <HomeCatering />
-    </main>
+    <>
+      <main className="min-h-screen flex flex-col bg-background text-primary">
+        <HomeNavbar />
+        <HomeHero />
+        <HomeLocations />
+        <HomeAbout />
+        <HomeReviews />
+        <HomeCatering />
+      </main>
+      <HomeFooter />
+    </>
   );
 }
