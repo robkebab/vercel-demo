fb6f2ae2747774fa6e86bbe5d45690c281bb65fb
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-06-30 17:53:01 -0500

    refactor(home): colocate files for home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-import { HomeAbout } from "@/components/home/about";
-import { HomeCatering } from "@/components/home/catering";
-import { HomeFooter } from "@/components/home/footer";
-import { HomeHero } from "@/components/home/hero";
-import { HomeLocations } from "@/components/home/locations";
-import { HomeNavbar } from "@/components/home/navbar";
-import { HomeReviews } from "@/components/home/reviews";
+import { HomeAbout } from "@/app/(home)/about";
+import { HomeCatering } from "@/app/(home)/catering";
+import { HomeFooter } from "@/app/(home)/footer";
+import { HomeHero } from "@/app/(home)/hero";
+import { HomeLocations } from "@/app/(home)/locations";
+import { HomeNavbar } from "@/app/(home)/navbar";
+import { HomeReviews } from "@/app/(home)/reviews";
 
 export default async function Home() {
   return (
-    <main className="min-h-screen flex flex-col bg-background text-primary">
-      <HomeNavbar />
-      <HomeHero />
-      <HomeLocations />
-      <HomeAbout />
-      <HomeReviews />
-      <HomeCatering />
+    <>
+      <main className="min-h-screen flex flex-col bg-background text-primary">
+        <HomeNavbar />
+        <HomeHero />
+        <HomeLocations />
+        <HomeAbout />
+        <HomeReviews />
+        <HomeCatering />
+      </main>
       <HomeFooter />
-    </main>
+    </>
   );
 }
