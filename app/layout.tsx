6a82aa2875925f9145374dbcc387088974fa497e
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-07-02 09:48:01 -0500

    feat(logs): add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Roboto, Roboto_Mono, Rubik_Distressed } from "next/font/google";
 import type { Metadata } from "next";
 import { ThemeProvider } from "../components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const robotoSans = Roboto({
   subsets: ["latin"],
@@ -42,6 +43,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
